26d8cc82b7eae45446859a31c62bbb78e5239abf
cyborgyn <cyborgyn@github.com>
2022-01-20 20:23:41 +0100

    Fix remove breakpoint physical address calculation

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 ASM=nasm
 
-$ASM -f bin CGATST.ASM -o CGATST.COM
-$ASM -f bin GDCTST.ASM -o GDCTST.COM
-$ASM -f bin CGA.ASM -o CGA.COM -dNASM=true
+if [ "$1" == "dosbox" ]; then
+    echo Building for dosbox...
+    $ASM -f bin CGA.ASM -o CGA.COM -dNASM=true -dDOSBOX=true
+else
+    echo Building all...
+    $ASM -f bin CGATST.ASM -o CGATST.COM
+    $ASM -f bin GDCTST.ASM -o GDCTST.COM
+    $ASM -f bin CGA.ASM -o CGA.COM -dNASM=true
+fi
